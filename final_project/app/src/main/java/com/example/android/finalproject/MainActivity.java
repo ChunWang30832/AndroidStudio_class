d7bac34bf0c661da9a82abc627e6b8346500a9aa
NTUTalan <t109590015@ntut.org.tw>
2022-06-12 14:43:22 +0800

    修改檔案名稱 & 移除滑動刪除項目

---
+++ final_project/app/src/main/java/com/example/android/finalproject/MainActivity.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.finalproject;
+
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/***
+ * Main Activity for the Material Me app, a mock sports news application.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    // Member variables.
+    private RecyclerView mRecyclerView;
+    private ArrayList<Item> mSportsData;
+    private ItemsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbarm);
+        setSupportActionBar(toolbar);
+
+        // Initialize the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the ArrayList that will contain the data.
+        mSportsData = new ArrayList<>();
+
+        // Initialize the adapter and set it to the RecyclerView.
+        mAdapter = new ItemsAdapter(this, mSportsData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Get the data.
+        initializeData();
+    }
+
+    /**
+     * Initialize the sports data from resources.
+     */
+    private void initializeData() {
+        // Get the resources from the XML file.
+        String[] sportsList = getResources()
+                .getStringArray(R.array.sports_titles);
+        String[] sportsInfo = getResources()
+                .getStringArray(R.array.sports_info);
+        TypedArray sportsImageResources = getResources()
+                .obtainTypedArray(R.array.sports_images);
+
+        // Clear the existing data (to avoid duplication).
+        mSportsData.clear();
+
+        // Create the ArrayList of Sports objects with the titles and
+        // information about each sport
+        for (int i = 0; i < sportsList.length; i++) {
+            mSportsData.add(new Item(sportsList[i], sportsInfo[i],
+                    sportsImageResources.getResourceId(i, 0)));
+        }
+
+        // Recycle the typed array.
+        sportsImageResources.recycle();
+
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
+    }
+
+    /////////////////////////////////////////////////////////////////////////////小弟打的///////////////////////////////////
+    private CharSequence menuIconWithText(Drawable r, String title) {
+        r.setBounds(0, 0, r.getIntrinsicWidth(), r.getIntrinsicHeight());
+        SpannableString sb = new SpannableString("    " + title);
+        ImageSpan imageSpan = new ImageSpan(r, ImageSpan.ALIGN_BOTTOM);
+        sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return sb;
+    }
+    /**程式中新增MenuItem選項*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /**itemId為稍後判斷點擊事件要用的*/
+        menu.add(0,0,0,"新增商品");
+        menu.add(0,1,1,"設定");
+        //menu.add(0,2,2,"第三選項");
+        /**setShowAsAction預設為NEVER
+         *MenuItem.SHOW_AS_ACTION_IF_ROOM 為如果Toolbar內還有空間，便會將這個item放到Toolbar內*/
+        //menu.add(0,4,4,"外部選項").setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return super.onCreateOptionsMenu(menu);
+    }
+    /**此處為設置點擊事件*/
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        /**取得Item的ItemId，判斷點擊到哪個元件*/
+        switch (item.getItemId()){
+            case 0:
+                Toast.makeText(this, "新增商品", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(this, "設定", Toast.LENGTH_SHORT).show();
+                break;
+            /*case 2:
+                Toast.makeText(this, "選三", Toast.LENGTH_SHORT).show();
+                break;
+            case 3:
+                Toast.makeText(this, "選帶ICON的Item", Toast.LENGTH_SHORT).show();
+                break;
+            case 4:
+                Toast.makeText(this, "選在外面的", Toast.LENGTH_SHORT).show();
+                break;*/
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+//////////////////////////////////////////////////////////////////////////////////////////小弟打的/////////////////////
+
+    /**
+     * onClick method for th FAB that resets the data.
+     *
+     * @param view The button view that was clicked.
+     */
+    public void goCart(View view) {
+        Intent intent = new Intent(this, cartActivity.class);
+        this.startActivity(intent);
+        //initializeData();
+    }
+}
